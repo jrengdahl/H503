4eabce07cc647f3b667eb71a8bbbeafe76bf5273
Jonathan Engdahl <engdahl@windstream.net>
2023-05-04 22:17:36 -0400

    change PLL setup to make it easier to change CPU frequency on the fly
    try to fix __NOINLINE in STM32H5xx_hal_def.h

--- Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_def.h
+++ Drivers/STM32H5xx_HAL_Driver/Inc/stm32h5xx_hal_def.h
@@ -35,6 +35,19 @@ extern "C" {
 #include <stddef.h>
 #include <math.h>
 
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+// hack to supress warning
+// because this file redefines __NOINLINE without a guard
+#ifdef __NOINLINE
+#undef __NOINLINE
+#endif
+
+/* USER CODE END PM */
+
+
+
 /* Exported types ----------------------------------------------------------------------------------------------------*/
 
 /**
@@ -212,9 +225,7 @@ typedef enum
 /* ARM & GNUCompiler
 
 */
-#ifndef __NOINLINE
 #define __NOINLINE __attribute__ ( (noinline) )
-#endif
 
 #elif defined ( __ICCARM__ )
 /* ICCARM Compiler
